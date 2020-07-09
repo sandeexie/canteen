68fcee791b88c2c4ba66f1ab246f8bc0329548cb
XieYunfei0921 <XieYunfei0921@126.com>
2020-07-09 22:01:40 +0800

    handler

---
+++ src/main/java/com/github/editor/handler/PostgreHandler.java
@@ -0,0 +1,35 @@
+package com.github.editor.handler;
+
+import com.github.editor.domain.UserInfo;
+import org.springframework.stereotype.Service;
+
+import java.sql.Connection;
+
+@Service
+public class PostgreHandler extends JDBCHandler{
+
+	public boolean checkAccess(UserInfo userInfo) {
+		return false;
+	}
+
+	public Connection getConnection() throws Exception {
+		return null;
+	}
+
+	public boolean preInspect() {
+		return false;
+	}
+
+	public Object execute(String sql) {
+		return null;
+	}
+
+	public Object execute(String sql, long deadline) {
+		return null;
+	}
+
+	public boolean releaseConnection() {
+		return false;
+	}
+
+}
---
+++ src/main/java/com/github/editor/handler/ResolveHandler.java
@@ -0,0 +1,5 @@
+package com.github.editor.handler;
+
+public interface ResolveHandler {
+
+}
