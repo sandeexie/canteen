e08364e4368472e35e9dc721a320ab9fc8c5fcc4
XieYunfei0921 <XieYunfei0921@126.com>
2020-07-13 21:54:19 +0800

    submit RPC client

---
+++ src/main/java/com/github/editor/rpc/RPCClient.java
@@ -0,0 +1,11 @@
+package com.github.editor.rpc;
+
+import java.util.concurrent.Future;
+
+public class RPCClient<T> {
+
+	public boolean remoteExecute(String host, Integer port, Future<T> task){
+		// TODO 之后执行
+		return true;
+	}
+}
