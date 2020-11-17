415112ca681612834589bf0a8012a638028f1d75
XieYunfei0921 <XieYunfei0921@126.com>
2020-11-17 15:24:28 +0800

    rpc地址封装

---
+++ src/main/java/com/github/canteen/rpc/RPCAddress.java
@@ -0,0 +1,40 @@
+package com.github.canteen.rpc;
+
+public class RPCAddress {
+
+	String host;
+
+	int port;
+
+	String protocol;
+
+	public RPCAddress(){
+		this.host="0.0.0.0";
+		this.port=0;
+		this.protocol="http";
+	}
+
+	public RPCAddress(int port){
+		this.host="127.0.0.1";
+		this.port=port;
+		this.protocol="http";
+	}
+
+	public RPCAddress(String host,int port){
+		this.host=host;
+		this.port=port;
+		this.protocol="http";
+	}
+
+	public RPCAddress(String host,int port,String protocol){
+		this.host=host;
+		this.port=port;
+		this.protocol=protocol;
+	}
+
+
+	@Override
+	public String toString() {
+		return this.protocol+"://"+this.host+":"+this.port;
+	}
+}
