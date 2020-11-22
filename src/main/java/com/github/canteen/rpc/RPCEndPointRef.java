0b6a272405052858ca3395ba7ffae4e2e9d5195e
XieYunfei0921 <XieYunfei0921@126.com>
2020-11-22 11:50:39 +0800

    RPC节点设计

--- src/main/java/com/github/canteen/rpc/RPCEndPointRef.java
+++ src/main/java/com/github/canteen/rpc/RPCEndPointRef.java
@@ -3,6 +3,11 @@ package com.github.canteen.rpc;
 import java.io.Serializable;
 import java.util.concurrent.Future;
 
+/**
+ * RPC通信实际的处理结构
+ * @author xyf
+ * @date 2020-11-21
+ */
 public abstract class RPCEndPointRef implements Serializable {
 
 	private int maxRetries;
@@ -18,12 +23,14 @@ public abstract class RPCEndPointRef implements Serializable {
 	// 发送单向异步消息
 	public abstract void send(Object message);
 
+	// 异步远端调用
 	public abstract Future ask(Object message,long timeout);
 
 	public Future ask(Object message){
 		return ask(message,defaultAskTimeout);
 	}
 
+	// 同步远端调用
 	public Object askSync(Object message,long timeout){
 		Future future = ask(message, timeout);
 		try {
